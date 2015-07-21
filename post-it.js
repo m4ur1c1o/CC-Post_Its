031f931695f9db89f56a1adeada3c0e96b90340a
codeamac01 <codeamac01@gmail.com>
2015-07-21 10:06:46 -0500

    Create class Board and PostIt. Create post-its on double click, edit and delete functionality.

--- post-it.js
+++ post-it.js
@@ -1,22 +1,69 @@
 var Board = function( selector ) {
-  // Aqui denerá ir el código que tenga que ver con tu tablero 
-  
-  // Utiliza esta sintaxis para referirte al selector que representa al tablero.
-  // De esta manera no dependerás tanto de tu HTML.  
-  var $elem = $( selector );
+  var self = this;
   
   function initialize() {
-    // Que debe de pasar cuando se crea un nuevo tablero?
+    self.$elem = $( selector );
+    self.$elem.dblclick(function(a) {
+      x = a.pageX + "px";
+      y = a.pageY + "px";
+      if (a.target == this) {
+        self.addPostIt(x, y);
+      };
+    });
+    self.deletePostIt();
   };
 
   initialize();
 };
 
-var PostIt = function() {
-  // Aquí va el código relacionado con un post-it
+Board.prototype.addPostIt = function(x, y){
+  var postIt = new PostIt();
+  postIt.$elem.css({
+    "position": "absolute",
+    "left": x,
+    "top": y
+  });
+  this.$elem.append(postIt.$elem);
 };
 
+Board.prototype.deletePostIt = function(){
+  var self = this;
+  this.$elem.on("click",".close", function(){
+    $(this).parent().parent().remove();
+  });
+};
+
+//////////////////////////////////////////////////////////////////
+var PostIt = function() {
+  // Aquí va el código relacionado con un post-it
+  var self = this;
+  string = "<div class='post-it'><div class='header'><div class='close'>X</div></div><div class='content' contentEditable='true'></div></div>";
+
+  function initialize () {
+    self.$elem = $(string);
+    self.$elem.draggable({
+      handle: "div.header"
+    });
+  };
+
+  PostIt.prototype.stopPropagation = function(){
+    $(".post-it").on("dblclick", function(event){
+      event.stopPropagation();
+    });
+  }
+  initialize();
+};  
+
+
+////////////////////////////////////////////////////////////////////
+
+
 $(function() {
-  // Esta es la fucnión que correrá cuando este listo el DOM 
-  new Board('#board');
+  // Esta es la función que correrá cuando este listo el DOM 
+  var board = new Board('#board');
+  
 });
+
+
+
+
